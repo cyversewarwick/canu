97153a21705b4f25489e263e24d79f5ff46016d5
Krzysztof Polanski <krzysztof_polanski@o2.pl>
2016-07-11 12:38:11 +0100

    canu

---
+++ scripts/canu_tarwrapper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+#mark start
+sleep 5
+touch tempfile
+sleep 5
+
+#run thing
+bash /canu-1.3/Linux-amd64/bin/canu -p canu -d canu_out "${@:1}"
+
+#wrap up output and kick out tempfile
+find . -mindepth 1 -newer tempfile -exec tar -rf FullOutput.tar {} \;
+rm tempfile
